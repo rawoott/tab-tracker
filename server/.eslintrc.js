6d78b5ba51e1bc85a1afd0159fe01186ebe32b75
rawoott <tofiquerawoot@gmail.com>
2022-10-02 11:43:24 +0200

    Setup of express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "plugin:vue/vue3-essential",
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
